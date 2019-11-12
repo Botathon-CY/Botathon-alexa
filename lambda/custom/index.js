f31d062ce8e3299c601c5c94e73a1fd804e1961d
James Alfei <james.alfei@dvla.gov.uk>
2019-11-12 13:30:26 +0000

    Hardcoded hospital name

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const https = require('https');
 const Alexa = require('ask-sdk-core');
 
 ////////////////////////////////
@@ -11,7 +12,7 @@ const LaunchRequestHandler = {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+        const speechText = 'Welcome to Hospital Parking. Where do you want to check parking?!';
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt(speechText)
@@ -34,18 +35,50 @@ const HelloWorldIntentHandler = {
     }
 };
 
+const DynamoIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'DynamoIntent';
+    },
+    handle(handlerInput) {
+        //TODO Dynamic endpoint based on what was said!!
+        https.get('https://i2yv4ll3q7.execute-api.eu-west-1.amazonaws.com/hack/space/current/morriston', (resp) => {
+            let data = '';
+
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            resp.on('end', () => {
+                if (resp.statusCode === 200) {
+                    try {
+                        const speechOutput = decodeParkingResponse(JSON.parse(data));
+                        return handlerInput.responseBuilder.speak(speechOutput);
+                    } catch (e) {
+                        return ErrorHandler.handle(handlerInput, {"message": "I'm having trouble understanding the response. Please try again later."})
+                    }
+                }
+
+                return ErrorHandler.handle(handlerInput, {"message": "I'm unable to get the data right now. Please try again later."})
+            });
+        }).on("error", (err) => {
+            return ErrorHandler.handle(handlerInput, err)
+        });
+    }
+};
+
 const HospitalParkingIntentHandler = {
-  canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-          && handlerInput.requestEnvelope.request.intent.name === 'HospitalParkingIntent';
-  },
-  handle(handlerInput) {
-      const speechText = 'There is a car park';
-      return handlerInput.responseBuilder
-          .speak(speechText)
-          .withSimpleCard('Parking', speechText)
-          .getResponse();
-  }
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'HospitalParkingIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'There is a car park';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Parking', speechText)
+            .getResponse();
+    }
 };
 
 const ErrorHandler = {
@@ -54,13 +87,33 @@ const ErrorHandler = {
     },
     handle(handlerInput, error) {
         console.log(`Error handled: ${error.message}`);
-        console.log(`Error stack: ${error.stack}`);
         return handlerInput.responseBuilder
             .speak("Sorry, I didn't understand. Please try again.")
             .reprompt("Sorry, I didn't understand. Please try again.")
             .getResponse();
     },
 };
+
+//////////////////////
+// Helper functions //
+//////////////////////
+
+function decodeParkingResponse(jsonData) {
+    console.log(jsonData);
+    const name = jsonData.name;
+    const areas = jsonData.parking_areas;
+    const total = jsonData.total_space;
+
+    let areaSpeech = "";
+    if (areas !== null && areas !== 'undefined') {
+        areas.forEach(function (area) {
+            const speech = "In the " + area.name + " car park, there are " + area.spaces + " spaces remaining.";
+            areaSpeech += speech;
+        });
+    }
+
+    return "In " + name + " hospital, there are " + total + " spaces remaining.\n\n" + areaSpeech;
+}
 
 ////////////////////////////////////
 // Set up handlers within the DSK //
@@ -71,7 +124,8 @@ exports.handler = skillBuilder
     .addRequestHandlers(
         LaunchRequestHandler,
         HelloWorldIntentHandler,
-        HospitalParkingIntentHandler
+        HospitalParkingIntentHandler,
+        DynamoIntentHandler
     )
     .addErrorHandlers(ErrorHandler)
     .lambda();
