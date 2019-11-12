91bbd139034c248d2a87ab88e8bc4c53d862a82c
FranM28 <fran_moore8@hotmail.com>
2019-11-12 18:16:49 +0000

    stuff

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,8 +2,11 @@
 
 const https = require('https');
 const Alexa = require('ask-sdk-core');
-const { getSlotValue } = require('ask-sdk-core');
 
+
+let hospitalName = 'morriston';
+
+const { getSlotValue } = require('ask-sdk-core');
 ////////////////////////////////
 // Code for the handlers here //
 ////////////////////////////////
@@ -21,7 +24,46 @@ const LaunchRequestHandler = {
     }
 };
 
-//{i want to|is there|are there|any|can i|how many|how much|}{park|parking|space|spaces|}{is|are|}{at|in|by|}{{hospital}}{hospital|}
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'Hello 1234 World!';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Hello 1234 World', speechText)
+            .getResponse();
+    }
+};
+
+const DynamoIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'DynamoIntent';
+    },
+    async handle(handlerInput) {
+        let speechOutput;
+        let textOutput;
+
+        await getCurrentSpaces(hospitalName)
+            .then((response) => {
+                console.log(response);
+                speechOutput = response.speech;
+                textOutput = response.text;
+            })
+            .catch((err) => {
+                ErrorHandler.handle(handlerInput, err);
+            });
+
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .withSimpleCard("Hospital parking", textOutput)
+            .getResponse();
+    }
+};
+
 const HospitalParkingIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
@@ -44,7 +86,36 @@ const HospitalParkingIntentHandler = {
 
         return handlerInput.responseBuilder
             .speak(speechOutput)
-            .withSimpleCard(textOutput.name + " hospital", textOutput.text)
+            .withSimpleCard("Hospital parking", textOutput)
+            .getResponse();
+    }
+};
+
+const HospitalPredictParkingIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'HospitalPredictParkingIntent';
+    },
+    async handle(handlerInput) {
+        let speechOutput;
+        let textOutput;
+        const hospitalName = getSlotValue(handlerInput.requestEnvelope, 'hospital');
+        const day = getSlotValue(handlerInput.requestEnvelope, 'day');
+        const time = getSlotValue(handlerInput.requestEnvelope, 'time');
+
+        await getPredictedSpaces(hospitalName, day, time)
+            .then((response) => {
+                console.log(response);
+                speechOutput = response.speech;
+                textOutput = response.text;
+            })
+            .catch((err) => {
+                ErrorHandler.handle(handlerInput, err);
+            });
+
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .withSimpleCard("Hospital parking", textOutput)
             .getResponse();
     }
 };
@@ -59,7 +130,7 @@ const ErrorHandler = {
             .speak("Sorry, I didn't understand. Please try again.")
             .reprompt("Sorry, I didn't understand. Please try again.")
             .getResponse();
-    }
+    },
 };
 
 //////////////////////
@@ -94,46 +165,68 @@ function getCurrentSpaces(hospital) {
     });
 }
 
-function decodeParkingResponseSpeech(jsonData) {
-    const  hospitalName = jsonData.name;
-    const areas = jsonData.parking_areas;
-    const totalSpaces = jsonData.total_space;
 
-    if (totalSpaces === 0) {
-        return "All car parking spaces are full at " + hospitalName + " hospital.";
-    }
+function getPredictedSpaces(hospital, dateTest, timeTime) {
+    return new Promise((resolve, reject) => {
+        https.get('https://i2yv4ll3q7.execute-api.eu-west-1.amazonaws.com/hack/space/predict/' + hospital + '/' + dateTest + 'T' + timeTime, (resp) => {
+            let data = '';
+
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+            resp.on('end', () => {
+                if (resp.statusCode === 200) {
+
+                    try {
+
+                        resolve({
+                            "speech": decodeParkingResponseSpeech(JSON.parse(data)),
+                            "text": decodeParkingResponseText(JSON.parse(data))
+                        });
+                    } catch (e) {
+                        reject({"message": "I'm having trouble understanding the response. Please try again later."});
+                    }
+                }
+                reject({"message": "I'm unable to get the data right now. Please try again later."});
+            });
+        }).on("error", (err) => {
+            reject(err);
+        });
+    });
+}
+
+function decodeParkingResponseSpeech(jsonData) {
+    const name = jsonData.name;
+    const areas = jsonData.parking_areas;
+    const total = jsonData.total_space;
 
     let areaSpeech = "";
     if (areas !== null && areas !== 'undefined') {
+
         areas.forEach(function (area) {
-            const speech = "In the " + area.name + " car park, there are " + area.spaces + " spaces. ";
+            const speech = "In the " + area.name + " car park, there are " + area.spaces + " spaces remaining. ";
             areaSpeech = areaSpeech + speech;
         });
     }
-    return "In " + hospitalName + " hospital, there are " + totalSpaces + " spaces remaining.\n\n" + areaSpeech;
+
+    return "In " + name + " hospital, there are " + total + " spaces remaining.\n\n" + areaSpeech;
 }
 
 function decodeParkingResponseText(jsonData) {
-    let hospitalName = jsonData.name;
-    hospitalName = hospitalName[0].toUpperCase() + hospitalName.slice(1);
+    const name = jsonData.name;
     const areas = jsonData.parking_areas;
-    const totalSpaces = jsonData.total_space;
-
-    if (totalSpaces === 0) {
-        return "All car parking spaces are full at " + hospitalName + " hospital.";
-    }
+    const total = jsonData.total_space;
 
     let areaSpeech = "";
     if (areas !== null && areas !== 'undefined') {
         areas.forEach(function (area) {
-            areaSpeech = areaSpeech + area.name + " car park: " + area.spaces + "\n";
+            areaSpeech = areaSpeech + area.name + ": " + area.space + "\n";
         });
     }
 
-    return {
-        "name": hospitalName,
-        "text": totalSpaces + " total spaces\n\n" + areaSpeech
-    };
+    return "Parking spaces in " + name + ": " + total + "\n\n" +
+        "Car parks: \n\n" +
+        areaSpeech;
 }
 
 ////////////////////////////////////
@@ -144,7 +237,10 @@ const skillBuilder = Alexa.SkillBuilders.custom();
 exports.handler = skillBuilder
     .addRequestHandlers(
         LaunchRequestHandler,
-        HospitalParkingIntentHandler
+        HelloWorldIntentHandler,
+        HospitalParkingIntentHandler,
+        HospitalPredictParkingIntentHandler,
+        DynamoIntentHandler
     )
     .addErrorHandlers(ErrorHandler)
     .lambda();
