62cf184c64e83ddbf0d44bf5d7bd34b69d352c22
James Alfei <james.alfei@dvla.gov.uk>
2019-11-12 11:24:37 +0000

    Updated tags

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,39 +1,64 @@
 'use strict';
+
 const Alexa = require('ask-sdk-core');
-// use 'ask-sdk' if standard SDK module is installed
+
 ////////////////////////////////
 // Code for the handlers here //
 ////////////////////////////////
-
-
-     const LaunchRequestHandler = {
-      canHandle(handlerInput) {
-          return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-      },
-      handle(handlerInput) {
-          const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
-  return handlerInput.responseBuilder
-              .speak(speechText)
-              .reprompt(speechText)
-              .withSimpleCard('Hello World', speechText)
-              .getResponse();
-      }
-  };
-
-const HelloWorldIntentHandler = {
-  canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-          && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
-  },
-  handle(handlerInput) {
-      const speechText = 'Hello World!';
-return handlerInput.responseBuilder
-          .speak(speechText)
-          .withSimpleCard('Hello World', speechText)
-          .getResponse();
-  }
-};
-
 exports.handler = Alexa.SkillBuilders.custom()
      .addRequestHandlers(LaunchRequestHandler, HelloWorldIntentHandler)
      .lambda();
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'Hello World!';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+        console.log(`Error stack: ${error.stack}`);
+        return handlerInput.responseBuilder
+            .speak("I'm sorry, I didn't understand. Please try again.")
+            .reprompt("I'm sorry, I didn't understand. Please try again.")
+            .getResponse();
+    },
+};
+
+////////////////////////////////////
+// Set up handlers within the DSK //
+////////////////////////////////////
+
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        HelloWorldIntentHandler
+    )
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
